b39cdd8b5a7c4f6e7a23a53f4a09699812f24652
don <don@localhost.localdomain>
2018-07-31 16:28:16 +0300

    finsihed the the content part

--- src/views/AfricaLeads.vue
+++ src/views/AfricaLeads.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="africa-leads">
-    <h1>This is Frica Leads page</h1>
+    <div class="banner">
+      <img src="@/assets/LOGO.png" alt="changa logo" class="logo">
+      <h3>Connect, <br> Collaborate, <br> Contribute, <br> <span>Credit.</span></h3> <br>
+      <img src="@/assets/WOMEN.png" alt="">
+      <h2>Africa <br>Project Leads</h2>  
+    </div>
+
+    <section class="main-content">
+      <p class="quote"> <span class="left"><i class="fas fa-quote-left"></i></span> <br>According to African Development Bank 2015 report, the informal sector contributes about 55% of Sub-Saharan Africa’s GDP and 80% of the labour force. </p>
+      <div class="column1">
+        <span class="dot"></span>
+      </div>
+      <p class="column2">
+        <span><h2>Selam Mussie <br> Ethiopia</h2></span>
+        Crowd Changa is a ‘Digital Chama’ that not only solves all the shortcomings associated with Chamas but also help them easily manage one without any financial knowledge. With analready established mobile base and a population sold to mobile money (Mpesa), Kenya is the most ideal flagship market for the platform. Crowd Changa assists individuals connect, collaborate, contribute, access credit. with the Crowd Changa, individuals can create the largest self-help community as easy as creating a what’s app group.
+      </p>
+    </section>
   </div>
 </template>
--- src/views/Solution.vue
+++ src/views/Solution.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="solution">
-    <h1>This is Solution page</h1>
+    <div class="banner">
+      <img src="@/assets/LOGO.png" alt="changa logo" class="logo">
+      <h3>Connect, <br> Collaborate, <br> Contribute, <br> <span>Credit.</span></h3> <br>
+      <img src="@/assets/MAN.png" alt="">
+      <h2>The Solution</h2>  
+    </div>
+
+    <section class="main-content">
+      <p class="quote"> <span class="left"><i class="fas fa-quote-left"></i></span> <br>According to African Development Bank 2015 report, the informal sector contributes about 55% of Sub-Saharan Africa’s GDP and 80% of the labour force. </p>
+      <p class="column1">
+        <span> Solution</span> <br>
+          Crowd Changa is a ‘Digital Chama’ that not only solves all the shortcomings associated with Chamas but also help them easily manage one without any financial knowledge. With analready established mobile base and a population sold to mobile money (Mpesa), Kenya is the most ideal flagship market for the platform. Crowd Changa assists individuals connect, collaborate, contribute, access credit. with the Crowd Changa, individuals can create the largest self-help community as easy as creating a what’s app group. The platform performs all the financial services including bank and mobile money remittances, payouts to members, setting meetings and financial goals, account statements, member and group contributions. According to African Development Bank 2015 report, the informal sector contributes about 55% of Sub-Saharan Africa’sGDP and 80% of the labour force. Despite the statistics,
+      </p>
+      <p class="column2">
+        most don't qualify for credit and those who do have to grapple with high-interest rates. In Kenya our government recently signed a bill to cap interest rates and financial institutions retaliated by cutting credit even further citing high risk exposure. Crowd Changa believes that credit should be a basic human right and only the people can privatize the resource. A community is as rich as the poorest person. The primary objective of Crowd Changa is to bring credit to the unbanked so that they empower themselves with basic necessities like water and energy eradicating poverty without leaving anyone behind. Crowd Changa is unique because it is based upon group guarantee. <br> <br>
+        <span>We believe that with the right supports we can reinforce this the concept digitally to solve poverty related problems across the world.</span>
+      </p>
+    </section>
   </div>
 </template>
